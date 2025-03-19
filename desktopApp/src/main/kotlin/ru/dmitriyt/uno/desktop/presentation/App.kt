0d28f2dd856879506627d73c4a1f2ddee7719354
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2025-03-19 23:44:35 +0400

    fix hover logic, add hover card offset

--- desktopApp/src/main/kotlin/ru/dmitriyt/uno/desktop/presentation/App.kt
+++ desktopApp/src/main/kotlin/ru/dmitriyt/uno/desktop/presentation/App.kt
@@ -1,5 +1,7 @@
 package ru.dmitriyt.uno.desktop.presentation
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -31,8 +34,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import ru.dmitriyt.uno.core.domain.model.Card
@@ -174,13 +179,16 @@ fun UnoBaseCard(
     val shape = RoundedCornerShape(16.dp)
     val interactionSource = remember { MutableInteractionSource() }
     val isHovered by interactionSource.collectIsHoveredAsState()
-    val scale = if (isHovered) 1.3f else 1f
+    val isHoverSelected = isHovered && onClick != null && enabled
+    val scaleAnimated by animateFloatAsState(if (isHoverSelected) 1.1f else 1f)
+    val yOffset = with(LocalDensity.current) { (if (isHoverSelected) 16.dp else 0.dp).toPx() }.toInt()
+    val yOffsetAnimated by animateIntAsState(yOffset)
     Box(
         modifier
-            .hoverable(interactionSource)
             .width(size.width)
             .height(size.height)
-            .scale(scale)
+            .scale(scaleAnimated)
+            .offset { IntOffset(0, -yOffsetAnimated) }
             .clip(shape)
             .background(
                 color = color,
@@ -191,6 +199,7 @@ fun UnoBaseCard(
                 color = Color.White,
                 shape = shape,
             )
+            .hoverable(interactionSource)
             .let { if (onClick != null && enabled) it.clickable { onClick() } else it },
     ) {
         Box(Modifier.fillMaxSize().padding(vertical = 4.dp, horizontal = 8.dp)) {
